f97219bef05bfd9bb2966e40747300121b05e639
Vincent Van Ghelue <vincent.vanghelue@gmail.com>
2025-10-07 00:27:47 +0200

    test module again

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Lambda! lol haha 8" }),
+    body: JSON.stringify({ message: "Hello from Lambda! lol haha 9" }),
   };
 };
 
