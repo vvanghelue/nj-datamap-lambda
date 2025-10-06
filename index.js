be5796145b8aa9676df906b0f7bab382b85f58e1
Vincent Van Ghelue <vincent.vanghelue@gmail.com>
2025-10-06 23:43:14 +0200

    v2 tests

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const handler = async (event) => {
     console.log(event.queryStringParameters);
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello from Lambda! lol" }),
+        body: JSON.stringify({ message: "Hello from Lambda! lol mdr" }),
     };
 };
 console.log("Lambda function initialized");
