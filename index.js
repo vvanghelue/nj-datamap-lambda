b8ac004e2308f4ca0cf8117a87a26c80eec789c3
Vincent Van Ghelue <vincent.vanghelue@gmail.com>
2025-10-07 00:20:15 +0200

    test module

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-export * from "./dist/index";
+//export * from "./dist/index";
+
+export const handler = async (event) => {
+  console.log('Event:', event);
+  return { statusCode: 200, body: 'Hello world' };
+};
